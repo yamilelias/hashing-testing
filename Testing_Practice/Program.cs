44bd03c1b90505e2859748f4703707e6f8cb9b6e
Yamil Elías <yamileliassoto@gmail.com>
2017-03-13 19:14:29 -0700

    Add project files.

---
+++ Testing_Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hash = "leepadg";
+
+            // Start hashing
+            var result = hashing(hash);
+            Console.WriteLine($"The result is {result}");
+        }
+
+        public static long hashing(string hash)
+        {
+            // Initialize variables
+            long h = 7;
+            var letters = "acdegilmnoprstuw";
+            var index = 0;
+            long inc = 0;
+
+            for(var k=0; k<hash.Length;k++)
+            {
+                inc = h * 37;
+                index = letters.IndexOf(hash[k]);
+                //index = hash.IndexOf(letters[k]);
+
+                h = inc + index;
+            }
+
+            return h;
+        }
+    }
+}
---
+++ Testing_PracticeTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing_Practice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Practice.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void hashingTest()
+        {
+            //Assert.Fail();
+            Assert.AreEqual(Program.hashing("leepadg"), 680131659347);
+        }
+    }
+}
